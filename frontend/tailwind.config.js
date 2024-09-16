639586ebd70defd79e632bb7b02a9e5a9e37bac6
vamshi nayak <136154466+vamshi-nayak@users.noreply.github.com>
2024-09-16 22:57:04 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,3 +7,14 @@ export default {
 	// eslint-disable-next-line no-undef
 	plugins: [require("daisyui")],
 };
+
+module.exports = {
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
+  },
+};
